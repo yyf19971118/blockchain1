48458d03e9af35cbf2048be20a29d65a64d0899b
yyf19971118 <1315891099@qq.com>
2020-02-10 12:41:35 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import pojo.Block;
+import pojo.BlockChain;
+
+public class Main {
+    public static void main(String[] args){
+        /*
+        Block block1 = new Block();
+        block1.setIndex(0);
+        block1.setTimestamp(1578000000000L);
+        block1.setHash("000c81784691c7bb8bacab6affb23312fd707bf0463e9afc217ce1a32ab3a4aa");
+        block1.setPreviousHash("0");
+        block1.setData("GENESIS BLOCK");
+        block1.setNonce(1545);
+
+        System.out.println(block1);
+
+        Block block2 = new Block();
+        block2.setIndex(1);
+        block2.setTimestamp(1578000000000L);
+        block2.setHash("000c81784691c7bb8bacab6affb23312fd707bf0463e9afc217ce1a32ab3a4aa");
+        block2.setPreviousHash("0");
+        block2.setData("GENESIS BLOCK");
+        block2.setNonce(1545);
+
+        System.out.println(block2);
+        System.out.println(block1.equals(block2));
+        System.out.println(block1.equals(null));
+        System.out.println(block2.equals(block1));
+        System.out.println(block2.equals(null));
+        System.out.println(block1.equals(block1));
+        System.out.println(block1.equals("abc"));
+        */
+        //BlockChain bc = new BlockChain();
+        //System.out.println(bc);
+        /*
+        //创建区块链对象
+        BlockChain bc = new BlockChain();
+        //生成新区块
+        Block block = bc.generateNextBlock("hello,blokchain.");
+        System.out.println(block);
+        //添加新区块
+        bc.addBlock(block);
+        //打印结果
+        System.out.println(bc);
+        */
+        BlockChain bc1 = new BlockChain();
+        bc1.addBlock(bc1.generateNextBlock("1-1"));
+        bc1.addBlock(bc1.generateNextBlock("1-2"));
+        bc1.addBlock(bc1.generateNextBlock("1-3"));
+        bc1.getBlockChain().get(2).setData("2-9");
+        BlockChain bc2 = new BlockChain();
+        bc2.addBlock(bc2.generateNextBlock("2-1"));
+        bc2.addBlock(bc2.generateNextBlock("2-2"));
+
+
+
+        System.out.println(bc1);
+        System.out.println(bc2);
+
+        System.out.println("执行替换");
+        bc2.replaceChain(bc1.getBlockChain());
+
+
+        System.out.println(bc1);
+        System.out.println(bc2);
+
+
+
+    }
+
+}
---
+++ src/main/java/pojo/Block.java
@@ -0,0 +1,155 @@
+package pojo;
+
+import com.google.common.base.Objects;
+
+/**
+ * 区块
+ * @author yangyifan
+ * @date 2020/2/5
+ */
+public class Block {
+    /**
+     * 索引–Index
+     * 时间戳–Timestamp
+     * 哈希值–Hash
+     * 上一个区块的哈希值-PreviousHash
+     * 数据–Data
+     * Nonce
+     * /
+     */
+
+    /**
+     * 索引
+     */
+    private int index;
+    /**
+     * 说明：
+     * public 是访问修饰符：表示当前修饰的变量、方法的访问权限
+     */
+    /**
+     * 时间戳
+     */
+    private long timestamp;
+
+    /**
+     * 哈希值
+     */
+    private String hash;
+    /**
+     * 上一区块的哈希值
+     */
+    private String previousHash;
+    /**
+     * 数据
+     */
+    private String data;
+    /**
+     * Nonce
+     */
+    private int nonce;
+    /**
+     * 无参构造方法
+     */
+    public Block() {
+
+    }
+
+    public Block(int index, long timestamp, String previousHash, String data) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        this.data = data;
+    }
+    public int getIndex(){
+        return index;
+    }
+    public void setIndex(int index){
+        this.index=index;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+    /**
+     * 获取计算哈希的原像字符串
+     * @return
+     */
+    public String originalString() {
+        return index + previousHash + timestamp + data + nonce;
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "index=" + index +
+                ", timestamp=" + timestamp +
+                ", ='" + hash + '\'' +
+                ", previousHash='" + previousHash + '\'' +
+                ", data='" + data + '\'' +
+                ", nonce=" + nonce +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        // 如果两个指向同一个对象，则相等
+        if (this == o) {
+            return true;
+        }
+        // 如果比较对象为空，则判定为不相等
+        if (o == null) {
+            return false;
+        }
+        // 如果比较对象和当前对象不是同一个类型，则判定为不相等
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        // 强制转换类型，并且对比它们的每一个属性是否相等
+        Block block = (Block) o;
+        return block.index == this.index &&
+                block.timestamp == this.timestamp &&
+                block.hash.equals(this.hash) &&
+                block.previousHash.equals(previousHash) &&
+                block.data.equals(data) &&
+                block.nonce == this.nonce;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(index, timestamp, hash, previousHash, data, nonce);
+    }
+}
